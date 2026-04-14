761e8bea42c67e02c48be93ff635fac9e36f8843
RayYamato <tmprpg@gmail.com>
2026-04-15 01:01:13 +0700

    Update a1.js

--- a1.js
+++ a1.js
@@ -1,5 +1,5 @@
 async function p4() {
-    const r = await fetch("https://rayyamato.github.io/ulx/4.txt")
+    const r = await fetch("https://rayyamato.github.io/ulx/4.txt");
     const t = await r.text();
     return t.trim();
 }
