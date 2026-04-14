e152ec6f490d65ce77834d1e9b8ee2f13bcbeccb
RayYamato <tmprpg@gmail.com>
2026-04-15 01:01:33 +0700

    Update c3.js

--- c3.js
+++ c3.js
@@ -1,5 +1,5 @@
 async function x25() {
-    const r = await fetch("https://drive.google.com/uc?export=download&id=1TU5jEJ3My4ZeRdvhFkK74GyVmUKDsklr");
+    const r = await fetch("https://rayyamato.github.io/ulx/25.txt");
     const t = await r.text();
     return t.trim();
 }
